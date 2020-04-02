b8d7de2efafe850841b349aa829f88ff96882532
stobias123 <stobias123@gmail.com>
2020-04-02 16:25:49 -0700

    adding main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ type tyHandler struct{}
 func (h tyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Printf("at=request path=%s", r.URL.Path)
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "ty!")
+	fmt.Fprintf(w, "wtfffffff ty!")
 }
 
 func main() {
